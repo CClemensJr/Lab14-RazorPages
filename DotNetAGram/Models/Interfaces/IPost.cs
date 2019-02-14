42cbf7656524e0320d0a5a95897076fef83e93f2
CClemensJr <cclemensjr@gmail.com>
2019-02-13 21:30:25 -0800

    Added method signatures to interface

--- DotNetAGram/Models/Interfaces/IPost.cs
+++ DotNetAGram/Models/Interfaces/IPost.cs
@@ -7,5 +7,19 @@ namespace DotNetAGram.Models.Interfaces
 {
     public interface IPost
     {
+        // Create
+        Task CreatePost(Post post);
+
+        // Read
+        Task<Post>GetPost(int id);
+
+        // ReadAll
+        Task<IEnumerable<Post>> GetPosts();
+
+        // Update
+        Task EditPost(int id);
+
+        // Destroy
+        Task DeletePost(int id);
     }
 }
