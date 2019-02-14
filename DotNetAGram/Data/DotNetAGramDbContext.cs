70492fd1296585b889fc8955a7828e9dae54b611
CClemensJr <cclemensjr@gmail.com>
2019-02-13 21:42:47 -0800

    Added DbContext dependency

--- DotNetAGram/Data/DotNetAGramDbContext.cs
+++ DotNetAGram/Data/DotNetAGramDbContext.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DotNetAGram.Data
 {
-    public class DotNetAGramDbContext
+    public class DotNetAGramDbContext : DbContext
     {
+
     }
 }
