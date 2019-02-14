46119b3ec1c3d890462955cdfb4067b51401ca1a
Charles Clemens <cclemensjr@gmail.com>
2019-02-14 12:14:32 -0800

    Added properties

--- DotNetAGram/Pages/Posts/Index.cshtml.cs
+++ DotNetAGram/Pages/Posts/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetAGram.Models;
 using DotNetAGram.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,8 +11,16 @@ namespace DotNetAGram.Pages.Posts
 {
     public class IndexModel : PageModel
     {
+        [FromRoute]
+        public int ID { get; set; }
+        public Post Post { get; set; }
+
         private readonly IPost _post;
 
+        /// <summary>
+        /// This constructor allows the IPost interface to be accessed in the Posts Index Controller
+        /// </summary>
+        /// <param name="post"></param>
         public IndexModel(IPost post)
         {
             _post = post;
