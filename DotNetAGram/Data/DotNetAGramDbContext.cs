e4e18fc14a20b2bab5d8eefa68b4d6a3d7e070df
CClemensJr <cclemensjr@gmail.com>
2019-02-13 21:48:27 -0800

    Added ModelCreating method

--- DotNetAGram/Data/DotNetAGramDbContext.cs
+++ DotNetAGram/Data/DotNetAGramDbContext.cs
@@ -8,6 +8,19 @@ namespace DotNetAGram.Data
 {
     public class DotNetAGramDbContext : DbContext
     {
+        /// <summary>
+        /// This constructor method brings in the DbContext options
+        /// </summary>
+        /// <param name="options"></param>
+        public DotNetAGramDbContext(DbContextOptions<DotNetAGramDbContext> options) : base(options) { }
 
+        /// <summary>
+        /// This method allows the DbContext to build model objects to seed the db
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
     }
 }
