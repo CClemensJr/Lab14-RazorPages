98120fa3d5b8bfb11cff977104e0229bbb1f7b67
Charles Clemens <cclemensjr@gmail.com>
2019-02-14 12:10:23 -0800

    Added post interface property

--- DotNetAGram/Pages/Posts/Index.cshtml.cs
+++ DotNetAGram/Pages/Posts/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetAGram.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,10 @@ namespace DotNetAGram.Pages.Posts
 {
     public class IndexModel : PageModel
     {
+        private readonly IPost _post;
+
+
+
         public void OnGet()
         {
         }
