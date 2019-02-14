8a49d7010acf3472b3a0cb26a31193b7c5b4d203
CClemensJr <cclemensjr@gmail.com>
2019-02-13 22:05:43 -0800

    Created 5 model objects

--- DotNetAGram/Data/DotNetAGramDbContext.cs
+++ DotNetAGram/Data/DotNetAGramDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DotNetAGram.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,47 @@ namespace DotNetAGram.Data
         /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Post>()
+                .HasData(
+                    new Post
+                    {
+                        ID = 1,
+                        Title = "Dogs in the Snow",
+                        Description = "Gunner and Yuna playing in snow at the dog park for the first time.",
+                        ImageURL = "https://placeimg.com/640/480/animals"
+                    },
+
+                    new Post
+                    {
+                        ID = 2,
+                        Title = "Dogs at the Beach",
+                        Description = "Gunner and Yuna playing at the beach for the first time.",
+                        ImageURL = "https://placeimg.com/640/480/animals"
+                    },
+
+                    new Post
+                    {
+                        ID = 3,
+                        Title = "Dogs in the Volcano",
+                        Description = "Gunner and Yuna playing in a volcano for the first time.",
+                        ImageURL = "https://placeimg.com/640/480/animals"
+                    },
+
+                    new Post
+                    {
+                        ID = 4,
+                        Title = "Dogs in the Jungle",
+                        Description = "Gunner and Yuna playing in the jungle for the first time.",
+                        ImageURL = "https://placeimg.com/640/480/animals"
+                    },
+
+                    new Post
+                    {
+                        ID = 5,
+                        Title = "Dogs in the Whitehouse",
+                        Description = "Gunner and Yuna playing in the White House for the first time.",
+                        ImageURL = "https://placeimg.com/640/480/animals"
+                    });
 
         }
     }
