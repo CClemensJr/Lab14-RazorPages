32720bbdbc0e9bc47cecd3091da0de4bcb0731a0
CClemensJr <cclemensjr@gmail.com>
2019-02-13 21:32:14 -0800

    Created post service

---
+++ DotNetAGram/Models/Services/PostService.cs
@@ -0,0 +1,36 @@
+﻿using DotNetAGram.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetAGram.Models.Services
+{
+    public class PostService : IPost
+    {
+        public Task CreatePost(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePost(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditPost(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetPost(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Post>> GetPosts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
