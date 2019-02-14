ed908d3c322e2918cab1516e7ce589cd91ad9005
CClemensJr <cclemensjr@gmail.com>
2019-02-13 21:49:09 -0800

    Updated url property

--- DotNetAGram/Models/Post.cs
+++ DotNetAGram/Models/Post.cs
@@ -10,6 +10,6 @@ namespace DotNetAGram.Models
         public int ID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string URL { get; set; }
+        public string ImageURL { get; set; }
     }
 }
