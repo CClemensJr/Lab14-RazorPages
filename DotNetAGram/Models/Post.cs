98f14449ebdefb58190e916ecb92e7efd453481c
Charles Clemens <cclemensjr@gmail.com>
2019-02-14 11:10:23 -0800

    Updated Post model to include author and post date

--- DotNetAGram/Models/Post.cs
+++ DotNetAGram/Models/Post.cs
@@ -8,8 +8,9 @@ namespace DotNetAGram.Models
     public class Post
     {
         public int ID { get; set; }
-        public string Title { get; set; }
+        public string Author { get; set; }
         public string Description { get; set; }
         public string ImageURL { get; set; }
+        public string PostDate { get; set; }
     }
 }
