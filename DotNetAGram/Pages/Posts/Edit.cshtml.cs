282fc544da9b0580d07590212097baccd208e81a
Charles Clemens <cclemensjr@gmail.com>
2019-02-18 11:02:19 -0800

    Added onPost method

--- DotNetAGram/Pages/Posts/Edit.cshtml.cs
+++ DotNetAGram/Pages/Posts/Edit.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using DotNetAGram.Models;
+using DotNetAGram.Models.Interfaces;
 
 namespace DotNetAGram.Pages.Posts
 {
@@ -25,7 +26,21 @@ namespace DotNetAGram.Pages.Posts
 
         public async Task OnGet()
         {
-            Post = await _post.GetPost(ID.GetValueOrDefault()) ?? new Posts();
+            Post = await _post.GetPost(ID.GetValueOrDefault()) ?? new Post();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var post = await _post.GetPost(ID.GetValueOrDefault()) ?? new Post();
+
+            post.Author = Post.Author;
+            post.PostDate = Post.PostDate;
+            post.ImageURL = Post.ImageURL;
+            post.Description = Post.Description;
+
+            await _post.EditPost(post);
+
+            return RedirectToPage("/Posts/Index", new { id = post.ID });
         }
     }
 }
