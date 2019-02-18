5670e06c2dd26578422874cd80f8feca86b4d814
Charles Clemens <cclemensjr@gmail.com>
2019-02-18 10:32:35 -0800

    added post property

--- DotNetAGram/Pages/Posts/Edit.cshtml.cs
+++ DotNetAGram/Pages/Posts/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DotNetAGram.Models;
 
 namespace DotNetAGram.Pages.Posts
 {
@@ -11,6 +12,9 @@ namespace DotNetAGram.Pages.Posts
     {
         [FromRoute]
         public int? ID { get; set; }
+
+        [BindProperty]
+        public Post Post { get; set; }
 
         public void OnGet()
         {
